c55ab736089ee5832442f04dc42ed1721f7b6fe3
Junmo <hongjm1022@gmail.com>
2024-10-14 15:33:15 +0900

    Initial commit

---
+++ src/main/java/CheersMate/cheersmate/CheersmateApplication.java
@@ -0,0 +1,13 @@
+package CheersMate.cheersmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheersmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheersmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CheersMate/cheersmate/CheersmateApplicationTests.java
@@ -0,0 +1,13 @@
+package CheersMate.cheersmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheersmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
